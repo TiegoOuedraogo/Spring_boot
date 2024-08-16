5e07dd10a7f74cafb75c1cd004e7834698ac4e03
tiegoouedraogo <tiego.ouedraogo@peopleshores.com>
2024-08-16 09:15:49 -0400

    Account repos updated

--- src/main/java/com/example/SprintPractice1/repos/AccountRepository.java
+++ src/main/java/com/example/SprintPractice1/repos/AccountRepository.java
@@ -7,5 +7,10 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    Account findByEmail(String email);
+    Account findByPhone(String phone);
+    Account findByAccountType(String accountType);
+    Account findByEmailAndAccountType(String email, String accountType);
+    Account findByPhoneAndAccountType(String phone, String accountType);
 }
 
