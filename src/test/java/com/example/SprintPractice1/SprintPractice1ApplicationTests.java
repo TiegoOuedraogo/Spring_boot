307d6c153e49efe65c0ed594f39a3689147b3961
tiegoouedraogo <tiego.ouedraogo@peopleshores.com>
2024-08-15 23:18:57 -0400

    first commit

---
+++ src/test/java/com/example/SprintPractice1/SprintPractice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SprintPractice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintPractice1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
